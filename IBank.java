80c0e072e67052d3cc688845856364803aaff228
Anil Kumar <sendkumaranil@gmail.com>
2016-06-22 14:28:15 +0530

    Update IBank.java

--- IBank.java
+++ IBank.java
@@ -1,8 +1,9 @@
 package org.springaopexamples.target;
 
 public interface IBank {
-	public double getBalance(long accounNo);
-	public void transfer(long creditAcountNo,long debitAccountNo);
+public double getBalance(long accounNo);
+	public void transfer(long creditAcountNo,long debitAccountNo,double amount);
 	public boolean isValidCard(long cardNo);
+	public void deposit(long accountno,double amount);
 
 }
